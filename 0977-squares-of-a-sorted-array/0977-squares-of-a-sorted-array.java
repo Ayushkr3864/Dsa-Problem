965a608867cd8527c9e5c758fd1bb3c037a46d2a
Ayush Kumar Ray <ayush.kumar.dev20@gmail.com>
2026-05-26 21:30:01 +0530

    Time: 1 ms (100%), Space: 48.3 MB (14.04%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -3,10 +3,41 @@ class Solution {
    
     public int[] sortedSquares(int[] nums) {
         int[] res = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            res[i] = nums[i]*nums[i];
+       int strt=0;
+     while(strt < nums.length && nums[strt] < 0){
+    strt++;
+}
+    int split = strt;
+       int[] neg = new int[split];
+         for (int i = 0; i < split; i++) {
+            neg[i] = nums[split - 1 - i] * nums[split - 1 - i];
         }
-        Arrays.sort(res);
+        int[]  pos = new int[nums.length-split];
+       for(int i=0;i<pos.length;i++){
+        pos[i] = nums[split+i]*nums[split+i];
+        strt++;
+       }
+       int i=0;
+       int j=0;
+       int id=0;
+       while(i<neg.length && j<pos.length){
+        if(neg[i]<pos[j]){
+            res[id] = neg[i];
+            i++;
+            id++;
+        }
+        else{
+            res[id] = pos[j];
+            j++;
+            id++;
+        }
+       }
+       while(i<neg.length){
+        res[id++] = neg[i++];
+       }
+       while(j<pos.length){
+        res[id++]= pos[j++];
+       }
         return res;
     }
 }
