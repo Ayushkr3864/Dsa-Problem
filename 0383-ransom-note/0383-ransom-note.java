5db86b710d10f0add5dc6eb46fa81a36c0a25c8f
Ayush Kumar Ray <ayush.kumar.dev20@gmail.com>
2026-07-27 20:32:20 +0530

    Time: 2 ms (56.43%), Space: 45.4 MB (99.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,24 +3,43 @@ class Solution {
         if(ransomNote.length()>magazine.length()){
             return false;
         }
-        HashMap<Character,Integer> mapMagazine = new HashMap<>();
-         HashMap<Character,Integer> mapRansom = new HashMap<>();
+        // HashMap<Character,Integer> mapMagazine = new HashMap<>();
+        //  HashMap<Character,Integer> mapRansom = new HashMap<>();
+        // for(char ch:magazine.toCharArray()){
+        //     mapMagazine.put(ch,mapMagazine.getOrDefault(ch,0)+1);
+        // }
+
+        // for(char ch:ransomNote.toCharArray()){
+        //     mapRansom.put(ch,mapRansom.getOrDefault(ch,0)+1);
+        // }
+
+        // for(char ch:ransomNote.toCharArray()){
+        //    if(!mapMagazine.containsKey(ch)){
+        //     return false;
+        //    }
+        //      if(mapRansom.get(ch)>mapMagazine.get(ch)){
+        //         return false;
+        //     }
+        //     }
+
+        char[] mag = new char[26];
+        char[] ran = new char[26];
         for(char ch:magazine.toCharArray()){
-            mapMagazine.put(ch,mapMagazine.getOrDefault(ch,0)+1);
+            mag[ch-'a']++;
         }
 
         for(char ch:ransomNote.toCharArray()){
-            mapRansom.put(ch,mapRansom.getOrDefault(ch,0)+1);
+            ran[ch-'a']++;
         }
 
         for(char ch:ransomNote.toCharArray()){
-           if(!mapMagazine.containsKey(ch)){
-            return false;
-           }
-             if(mapRansom.get(ch)>mapMagazine.get(ch)){
+            if(mag[ch-'a']==0){
                 return false;
             }
+            else if(ran[ch-'a']>mag[ch-'a']){
+                return false;
             }
+        }
           
         
         return true;
